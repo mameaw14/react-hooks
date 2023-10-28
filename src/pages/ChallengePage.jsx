19fc035b6ff81bf464322a11c8c351ad64fdb466
Aod <acspdoctoraod@gmail.com>
2023-10-28 15:03:45 +0700

    feat challenge

--- src/pages/ChallengePage.jsx
+++ src/pages/ChallengePage.jsx
@@ -1,12 +1,19 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import generateText from '../utils/generateText'
 
 const useTextDecryption = (data, interval = 3000) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      setCurrentIndex(index => {
+        if ((index + 1) === 3) return 0
+        return index + 1
+      });
+    }, (interval));
+    return () => clearInterval(intervalRef);
+  }, [interval])
 
-// TODO: rotate index every interval
-// TODO: generate random text (hint: need another hook)
-
-  return [data[currentIndex], '']
+  return [data[currentIndex]]
 }
 
 export const ChallengePage = () => {
@@ -15,12 +22,28 @@ export const ChallengePage = () => {
     "share encrypted data",
     "encrypt credit cards",
   ];
-  const [text, randomText] = useTextDecryption(data);
+  const [text] = useTextDecryption(data);
+  const [renderText, setRenderText] = useState("")
+  const [renderLastText, setRenderLastText] = useState("")
+  useEffect(() => {
+    function timeout(delay) {
+      return new Promise( res => setTimeout(res, delay) );
+    }
+    async function render(input) {
+      for (let i = 0;i < input.length;i++) {
+        await timeout(50)
+        setRenderText(prev => prev + input[i])
+        setRenderLastText(generateText(input.length - 1 - i))
+      }
+    }
+    setRenderText("")
+    render(text)
+  }, [text])
+  
   return (
     <div>
-      <h1>
-        {text}{randomText}
-      < /h1>
+
+      <h1>{renderText} {renderLastText}</h1>
     </div>
   )
 }
