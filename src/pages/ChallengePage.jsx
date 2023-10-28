289e087f362ef96b1ef3d3ec5d3733f14046854d
Mameaw <pinyapornster@gmail.com>
2023-10-28 11:58:59 +0700

    update challenge

--- src/pages/ChallengePage.jsx
+++ src/pages/ChallengePage.jsx
@@ -1,12 +1,47 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
+function randomString(length) {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++)
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  return result;
+}
+
+const useRandomText = (text) => {
+  const [index, setIndex] = useState(1)
+  const [randomText, setRandomText] = useState(randomString(text.length - 1))
+  const indexRef = useRef(1)
+
+  useEffect(() => {
+    setIndex(1)
+    indexRef.current = 1
+    const interval = setInterval(() => {
+      setIndex(prev => prev + 1)
+      indexRef.current = Math.min(indexRef.current + 1, text.length)
+      setRandomText(randomString(text.length - indexRef.current))
+    }, 50)
+
+    return () => clearInterval(interval)
+  }, [text]);
+
+  return [text.slice(0, index), randomText]
+}
 const useTextDecryption = (data, interval = 3000) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [text, randomText] = useRandomText(data[currentIndex])
 
-// TODO: rotate index every interval
-// TODO: generate random text (hint: need another hook)
+  useEffect(() => {
+    const length = data.length
+    const timeout = setTimeout(() => {
+      setCurrentIndex(prevState => (prevState + 1) % length)
+    }, interval)
+    return () => clearTimeout(timeout)
+  }, [currentIndex]);
 
-  return [data[currentIndex], '']
+  return [text, randomText]
 }
 
 export const ChallengePage = () => {
